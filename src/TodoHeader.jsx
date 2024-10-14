cdc36e846d97dd3923a9d75fc86b32fcb3fcbc88
Yoonjeong Ko <jejukyj@naver.com>
2024-10-14 18:50:56 +0900

    5주차 퀴즈 제출

--- src/TodoHeader.jsx
+++ src/TodoHeader.jsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 // TodoHeader의 색상이 올바르게 업데이트되도록 수정해주세요.
 // 힌트: 필요 없는 state는 제거하세요. 
 export default function TodoHeader(props) {
-  const [color, setColor] = useState(props.color);
   return (
     <div> 
-      <h1 style={{ color: color, textAlign: "center" }}>Todo List</h1>
+      <h1 style={{ color: props.color, textAlign: "center" }}>Todo List</h1>
     </div>
   );
 }
+// color props 직접 사용
