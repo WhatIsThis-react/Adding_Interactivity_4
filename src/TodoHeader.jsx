48cfe7b6cbc4a892a3948a7829558046fa6409b6
hyorish03 <hyorish2001@gmail.com>
2024-10-14 18:45:11 +0900

    Feat: Header에 불필요한 state 삭제 및 색 선택 시 header의 색이 변경되도록 수정

--- src/TodoHeader.jsx
+++ src/TodoHeader.jsx
@@ -1,13 +1,12 @@
+/* eslint-disable react/prop-types */
 //Todo.jsx
-import { useState } from "react";
 
 // 문제 1.
 // TodoHeader의 색상이 올바르게 업데이트되도록 수정해주세요.
-// 힌트: 필요 없는 state는 제거하세요. 
-export default function TodoHeader(props) {
-  const [color, setColor] = useState(props.color);
+// 힌트: 필요 없는 state는 제거하세요.
+export default function TodoHeader({ color }) {
   return (
-    <div> 
+    <div>
       <h1 style={{ color: color, textAlign: "center" }}>Todo List</h1>
     </div>
   );
